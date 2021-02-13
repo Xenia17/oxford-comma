dff902faa1713d1b82fd2857495153da04a7b908
Ksenia serdyukova <kingsbroo@gmail.com>
2021-02-12 22:43:25 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
 
+
+def oxford_comma(array)
+    if array.length >= 3
+    array.last.insert(0, "and ")
+    array.join(", ")
+    elsif array.length == 2
+        array.join(" and ")
+    else 
+    array.join(", ") 
+    
+    end
 end
